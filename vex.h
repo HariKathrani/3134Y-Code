5af92d6a0f4b9f6b7a63583373c55936ac210402
HariKathrani <99310687+HariKathrani@users.noreply.github.com>
2023-10-14 00:19:51 -0700

    Create vex.h

---
+++ vex.h
@@ -0,0 +1,15 @@
+using namespace vex;
+
+extern brain Brain;
+extern drivetrain Drivetrain;
+extern motor Intake;
+extern digital_out DigitalOutA;
+extern digital_out DigitalOutB;
+extern digital_out DigitalOutC;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ *
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit(void);
