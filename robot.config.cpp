4223f2d6bf0f79c1fbdbaf74bc9ae9f811df3c0c
HariKathrani <99310687+HariKathrani@users.noreply.github.com>
2024-01-13 22:05:23 -0800

    Update robot.config.cpp

--- robot.config.cpp
+++ robot.config.cpp
@@ -1,15 +1,25 @@
 #include "vex.h"
 
 using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
 
-// A global instance of brain used for printing to the V5 brain screen
-brain Brain;
+// define a task that will handle monitoring inputs from Controller1
+int rc_auto_loop_function_Controller1() {
+  // process the controller input every 20 milliseconds
+  // update the motors based on the input values
+  while(true) {
+    // wait before repeating the process
+    wait(20, msec);
+  }
+  return 0;
+}
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- *
+ * 
  * This should be called at the start of your int main function.
  */
-void vexcodeInit(void) {
-  // Nothing to initialize
+void vexcodeInit( void ) {
+  task rc_auto_loop_task_Controller1(rc_auto_loop_function_Controller1);
 }
