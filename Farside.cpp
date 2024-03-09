944f2ac3460950cd92e9b0d618576ee13999c50a
HariKathrani <99310687+HariKathrani@users.noreply.github.com>
2024-03-09 15:59:00 -0800

    Update Farside.cpp

--- Farside.cpp
+++ Farside.cpp
@@ -1,14 +1,37 @@
 // Farside Auton
 
 void autonomus(void) {
+  //Setting Drive Velocity
   Drivetrain.setVelocity(100, pct);
   Intake.setVelocity(80, pct);
 
-  Drivetrain.driveFor(forward, 38, inches);
-  Intake.spinFor(forward, 2, seconds);
+  Drivetrain.drive(forward);
+  wait(4, seconds);
 
-  Intake.spinFor(reverse, 1, second)
+  Drivetrain.setRotation(180, deg);
 
-  Drivetrain.driveFor(reverse, 3, inches);
-  Drivetrain.setRotation(
+  Drivetrain.drive(forward);
+  Intake.spin(reverse);
+  wait(1, seconds);
+
+  Drivetrain.drive(reverse);
+  wait(50, msec);
+
+  Drivetrain.setRotation(270, deg);
+
+  Drivetrain.drive(forward);
+  Intake.spin(forward);
+  wait(3, seconds);
+
+  Intake.stop()
+  Drivetrain.setRotation(210, deg);
+
+  Drivetrain.drive(forward);
+  wait(3, seconds);
+
+  Intake.spin(reverse);
+  wait(1, second);
+
+  Drivetrain.stop()
+  Intake.stop()
 }
